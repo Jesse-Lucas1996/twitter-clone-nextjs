48bd292d40c725a0c04b50f6066c4205b2e24edf
JesseLucas1996 <128339106+JesseLucas1996@users.noreply.github.com>
2023-06-13 05:35:52 +0800

    fixed component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,31 @@
-import styles from '@/styles/Home.module.css';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import CreatePostPage from './components/posts';
-import PostList  from './components/getPosts';
+import styles from "@/styles/Home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
+import CreatePostPage from "./components/posts";
+import PostList from "./components/getPosts";
 
 export default function Home() {
   const { data: session } = useSession();
   if (session) {
     return (
       <main className={styles.container}>
-        <div className={styles.title}> text goes here </div>
-        <div className={styles.content}> content goes here </div>
-        <button className={styles.signin} onClick={() => signOut()}>Sign out</button>
+        <button className={styles.signin} onClick={() => signOut()}>
+          Sign out
+        </button>
+        <CreatePostPage />
+        <div style={{ marginBottom: "16px" }}></div>{" "}
+        {/* Add this div for spacing */}
+        <PostList />
       </main>
     );
   } else {
     return (
       <main className={styles.container}>
-      <button className={styles.signin} onClick={() => signOut()}>Sign out</button>
-      <CreatePostPage />
-      <div style={{ marginBottom: '16px' }}></div> {/* Add this div for spacing */}
-      <PostList />
-    </main>
+        <div className={styles.title}> text goes here </div>
+        <div className={styles.content}> content goes here </div>
+        <button className={styles.signin} onClick={() => signOut()}>
+          Sign out
+        </button>
+      </main>
     );
   }
 }
