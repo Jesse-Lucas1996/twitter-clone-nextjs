7e0f8fe5ab62c70469b95df0b160f620bdb7d524
JesseLucas1996 <128339106+JesseLucas1996@users.noreply.github.com>
2023-06-13 17:12:30 +0800

    added session to get posts

--- src/pages/api/getPosts.ts
+++ src/pages/api/getPosts.ts
@@ -1,8 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../lib/mongodb';
+import { getServerSession } from 'next-auth/next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
+  const session = await getServerSession(req, res, {});
+  if (!session || !session.user) {
+    return res.status(403).json({message:"No session found"});
+  }  try {
     const client = await connectToDatabase();
     const db = client.db();
     const posts = await db.collection('posts').find().toArray();
