4e036823537ab931559b93cf190ec81823f34e84
jesse-lucas1996 <jessejlucas@Hotmail.com>
2023-06-18 23:36:16 +0800

    added viewport tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>Not Twitter</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body>
         <Main />
         <NextScript />
